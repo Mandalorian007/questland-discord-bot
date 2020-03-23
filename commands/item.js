377f7840d37c2fb789105520d1f26e7b93478eda
Loraykin <62488390+Loraykin@users.noreply.github.com>
2020-03-23 23:07:05 +1100

    Autocomplete for item search, static item name array (#3)
    * Autocomplete for item search, static item name array
    
    * Sort suggestions by significance
    
    * Autocomplete for orbs (copy/paste from item.js)
    Using pure URL instead of attachment for message thumbnail.
    'Multiple results'/'no results' messages moved to commands/_helper.js
    
    * Moved messages to own helper

--- commands/item.js
+++ commands/item.js
@@ -1,6 +1,8 @@
 const fetch = require("node-fetch");
 const Discord = require("discord.js");
-const { asyncHandler } = require("./_helper");
+const { asyncHandler } = require("./_helper")
+const { multipleResultsFoundMessage, noResultFoundMessage } = require("../helpers/messageHelper");
+
 
 exports.command = 'item';
 exports.describe = 'Get details about a Questland Item';
@@ -21,7 +23,7 @@ exports.builder = (yargs) => {
 
 exports.handler = asyncHandler(async (argv) => {
   if (argv.h) {
-    return`Usage: !ql item <item name> [options]
+    return `Usage: !ql item <item name> [options]
 
 Commands:
   !ql item  Get details about a Questland Item
@@ -38,24 +40,71 @@ Examples:
 
   let temp = argv._;
   temp = temp.filter(x => x !== 'item');
-  const itemName = temp.join(' ');
+  let itemName = temp.join(' ');
+
+  // check item name against local static array. 
+  let candidates = matchItemName(itemName);
+
+  // only process further if matchItemName returned an array
+  if (Object.prototype.toString.call(candidates) === '[object Array]') {
+    if (candidates.length == 1) {
+      // unambiguous match, replace input (autocomplete)
+      itemName = candidates[0];
+    }
+    else if (candidates.length > 1) {
+      // multiple matches, suggest some candidates
+      // prioritise items which start with the input term
+      candidates = candidates.sort((a, b) => a.toLowerCase().indexOf(itemName.toLowerCase())
+        - b.toLowerCase().indexOf(itemName.toLowerCase()))
+
+      // limit number of suggestions
+      const maxCandidates = 5;
+      let suggestions = candidates.slice(0, maxCandidates);
+
+      // append ellipsis, if not all matches are shown as suggestions
+      if (suggestions.length < candidates.length)
+        suggestions.push('...');
+
+      // create suggestions output
+      suggestions = suggestions.join('\n');
+
+      // just show suggestions, no API call
+      return multipleResultsFoundMessage(itemName, suggestions);
+    }
+  }
 
   let param = '';
   if (argv.a) {
-    param = `?quality=ARTIFACT${ argv.a }`;
+    param = `?quality=ARTIFACT${argv.a}`;
   }
 
-  let url = 'https://questland-public-api.cfapps.io/items/name/'
+  const url = 'https://questland-public-api.cfapps.io/items/name/'
     + encodeURIComponent(itemName)
     + param;
   const response = await fetch(url);
-  return response.ok ? printItem(await response.json()) : 'Unable to locate item.';
+  return response.ok ? printItem(await response.json()) : noResultFoundMessage(itemName, 'Item');
 });
+
+// match an item name against the static array of available item names
+// TODO: make dynamic, call API then cache results, or use local DB which gets periodically updated
+const matchItemName = (name) => {
+  try {
+    // get static array of item names
+    const module = require('../data/itemNames.js');
+    const itemNames = module.itemNames;
+
+    // filter by name input
+    return itemNames.filter(i => i.toLowerCase().includes(name.toLowerCase()));
+  } catch (e) {
+    console.error(e);
+    return 'Unable to resolve item name.';
+  }
+}
 
 const printItem = (item) => {
   try {
     let embed = new Discord.RichEmbed()
-      .setTitle(`${ item.name }`)
+      .setTitle(`${item.name}`)
       .addField('Potential (atk, mag, def, hp)',
         '' + item.totalPotential
         + ' (' + item.attackPotential
@@ -84,4 +133,4 @@ const printItem = (item) => {
     console.error(e);
     return 'Unable to format item data.';
   }
-};
+}
