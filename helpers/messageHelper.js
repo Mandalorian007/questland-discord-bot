c4ec6cc69624b14ba3f2a56a9385db8203d762bf
mcf <mcf@spotify.com>
2020-05-08 21:00:18 -0400

    add confused image and a test unknown command boilerplate

--- helpers/messageHelper.js
+++ helpers/messageHelper.js
@@ -13,3 +13,11 @@ exports.noResultFoundMessage = (searchTerm, searchCategory) =>
     .setTitle(`${searchCategory} '${searchTerm}' not found`)
     .addField('Please check your input', '\u200b')
     .setThumbnail('https://questland-discord-bot.cfapps.io/noidea.png');
+
+// Discord message "unknown option value"
+exports.unknownServerMessage = (option, optionAlias, submitted, allowedValues) =>
+  new Discord.RichEmbed()
+    .setTitle(`Unable able parse: ${option}(${optionAlias})`)
+    .addField('You sent:', `${submitted}`)
+    .addField('Available Options:', `${allowedValues.join(', ')}`)
+    .setThumbnail('https://questland-discord-bot.cfapps.io/noidea.png');
