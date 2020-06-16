1f16cef84f26ed23b940cc446521355ffd604e5a
mcf <mcf@spotify.com>
2020-06-16 07:18:04 -0400

    add monster slayer support!

---
+++ commands/monster-slayer.js
@@ -0,0 +1,68 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const { asyncHandler } = require("./_helper");
+const { serverMatcher, serverOptions } = require("../helpers/optionHelper");
+const { optionNotFoundMessage, helpMessage } = require("../helpers/messageHelper");
+const { dailyStandard, whiteBuster, intenseSwordWielding, flamingShield } = require("./../data/dailyBuilds");
+
+exports.command = 'monster-slayer';
+exports.describe = 'Get stage scores for Monster Slayer';
+exports.builder = (yargs) => {
+  return yargs
+    .option('h', {
+      alias: 'help',
+      demandOption: false,
+      describe: 'Show Help'
+    })
+};
+
+exports.handler = asyncHandler(async (argv) => {
+  if (argv.h) {
+    return getHelpMessage();
+  }
+
+  const response = await fetch('https://questland-public-api.cfapps.io/monster-slayer');
+  const monsterSlayer = response.ok ? await response.json().then(data => data.stageScores) : null;
+
+  return printScores(monsterSlayer);
+});
+
+const printScores = (monsterSlayer) => {
+  try {
+    const embed = new Discord.RichEmbed()
+        .setTitle(`Monster Slayer`)
+        .setThumbnail('https://questland-discord-bot.cfapps.io/monster-slayer.png')
+        .setFooter('Love QL Bot? Check out the about command for more good stuff!',
+                'https://questland-discord-bot.cfapps.io/ql_weasel.png');
+    if(monsterSlayer) {
+        var scores = [];
+        for (i = 0; i < 10; i++) {
+            var data = monsterSlayer[i];
+            scores.push(`${data.stageScore} | ${data.locationName} - ${data.stageName} (${data.locationNumber}-${data.stageNumber})`);
+        }
+
+        embed.setDescription(`Stage Scores \n\`\`\`${scores.join('\n')}\`\`\``)
+    } else {
+        embed.addField(
+        'No active monster slayer quest found',
+        'If you would like to calculate the quest manually please visit: [Monster Slayer Calculator](https://questland-handbook.cfapps.io/tools/monster-slayer-calc)',
+        false);
+    }
+
+    return { embed };
+  } catch (e) {
+    console.error(e);
+    return 'Unable to format monster slayer data.';
+  }
+};
+
+const getHelpMessage = () =>
+  helpMessage(
+    'monster-slayer',
+    'Used to get the level scores for the Monster Slayer Quest Event',
+    '`!ql monster-slayer`',
+    [],
+    [
+      '`!ql monster-slayer'
+    ]
+  );
