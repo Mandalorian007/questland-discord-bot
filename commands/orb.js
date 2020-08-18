8c19c89f09ad332ac525b1adf0a7200e14912434
mcf <mcf@spotify.com>
2020-08-18 08:29:10 -0400

    update orb so it can give stats at a specific level!

--- commands/orb.js
+++ commands/orb.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 const Discord = require("discord.js");
 const { asyncHandler } = require("./_helper")
-const { multipleResultsFoundMessage, noResultFoundMessage, helpMessage } = require("../helpers/messageHelper");
+const { optionNotFoundMessage, multipleResultsFoundMessage, noResultFoundMessage, helpMessage } = require("../helpers/messageHelper");
 const { cacheService } = require("../helpers/cache");
 
 const ttl = 60 * 60; // cache for 1 Hour
@@ -31,11 +31,16 @@ exports.handler = asyncHandler(async (argv) => {
       'Used to get detailed stats about an in game orb at legendary or artifact level.',
       '`!ql orb <orb name> [options]`',
       [
-        '`-a, --artifact` Choose an artifact level [choices: 1]'
+        '`-a, --artifact` Choose an artifact level [choices: 1]',
+        '`-l, --level` Select the orb level. Default is 1 [max legendary: 100, max artifact: 180]',
+        '`-e, --enhance` Choose an enhancement level. Default is 0 [choices: 0-10]'
       ],
       [
         '`!ql orb Behemoth Flames` Get the details for Behemoth Flames orb.',
-        '`!ql orb Requiem -a 1` Get the details for Requem orb at Artifact level 1.'
+        '`!ql orb Requiem -a 1` Get the details for Requem orb at Artifact level 1.',
+        '`!ql orb Requiem -l 100` Get the details for Requem orb with stats at level 100.',
+        '`!ql orb Requiem -l 100 -e 5` Get the details for Requem with stats at level 100 and enhanced to level 5.',
+        '`!ql orb Requiem -a 1 -l 180 -e 5` Get the details for Requem artifact orb with stats at level 180 and enhanced to level 5.'
       ]
     )
   }
@@ -81,13 +86,36 @@ exports.handler = asyncHandler(async (argv) => {
   if (argv.a) {
     param = `?quality=ARTIFACT${ argv.a }`;
   }
+  let enhance = 0;
+  if (argv.e) {
+    const desiredEnhance = parseInt(argv.e);
+    if (desiredEnhance > 10 || desiredEnhance < 0) {
+      return optionNotFoundMessage('e', 'enhance', argv.e, [0,1,2,3,4,5,6,7,8,9,10])
+    }
+    enhance = desiredEnhance;
+  }
+  let level = 1;
+  if (argv.l) {
+    const desiredLevel = parseInt(argv.l);
+    if (desiredLevel < 1 || (argv.a && desiredLevel > 180) || (!argv.a && desiredLevel > 100)) {
+      return optionNotFoundMessage('l', 'level', argv.l, ['legendary 1-100', 'artifact 1-180'])
+    }
+    level = desiredLevel;
+  }
 
   const url = 'https://questland-public-api.cfapps.io/orbs/name/'
     + encodeURIComponent(orbName)
     + param;
   const response = await fetch(url);
 
-  return response.ok ? printOrb(await response.json()) : noResultFoundMessage(orbName, 'Orb');
+  if (response.ok) {
+    const orbData = await response.json();
+    const scaledOrb = scaleOrb(orbData, enhance, level);
+
+    return printOrb(scaledOrb);
+  } else {
+    return noResultFoundMessage(orbName, 'Orb');
+  }
 });
 
 // function for retrieving a list of orb names
@@ -132,22 +160,54 @@ const smarten = (text) => {
   return text
 };
 
-const printOrb = (orb) => {
+const scaleOrb = (orb, enhance, level) => {
+  orb.healthPotential = scalePotential(orb.healthPotential, enhance);
+  orb.attackPotential = scalePotential(orb.attackPotential, enhance);
+  orb.defensePotential = scalePotential(orb.defensePotential, enhance);
+  orb.magicPotential = scalePotential(orb.magicPotential, enhance);
+
+  orb.health = scaleBaseStat(orb.health, enhance, orb.healthPotential, level)
+  orb.attack = scaleBaseStat(orb.attack, enhance, orb.attackPotential, level)
+  orb.defense = scaleBaseStat(orb.defense, enhance, orb.defensePotential, level)
+  orb.magic = scaleBaseStat(orb.magic, enhance, orb.magicPotential, level)
+
+  orb.enhance = enhance;
+  orb.level = level;
+
+  return orb;
+}
+
+const scalePotential = (potential, enhance) => {
+  if (potential < 1) {
+    return potential;
+  }
+  return potential + (potential * (.05 * enhance));
+
+}
+const scaleBaseStat = (baseStat, enhance, enhancedPotential, level) => {
+  if (baseStat < 1) {
+    return baseStat;
+  }
+  const enhancedBaseStat = baseStat + (baseStat * (.05 * enhance));
+  return enhancedBaseStat + (enhancedPotential * (level - 1));
+}
+
+const printOrb = (scaledOrb) => {
   try {
     const embed = new Discord.RichEmbed()
-      .setTitle(`${ orb.name }`)
-      .addField('Potential (hp, atk, def, mag)',
-        '' + orb.healthPotential
-        + ', ' + orb.attackPotential
-        + ', ' + orb.defensePotential
-        + ', ' + orb.magicPotential,
+      .setTitle(`${ scaledOrb.name }`)
+      .addField(`Potential at enhance ${scaledOrb.enhance} (hp, atk, def, mag)`,
+        '' + scaledOrb.healthPotential
+        + ', ' + scaledOrb.attackPotential
+        + ', ' + scaledOrb.defensePotential
+        + ', ' + scaledOrb.magicPotential,
         false)
-      .addField('Quality', orb.quality, false)
-      .addField('Stats (hp, atk, def, mag)',
-        '' + orb.health
-        + ', ' + orb.attack
-        + ', ' + orb.defense
-        + ', ' + orb.magic,
+      .addField('Quality', scaledOrb.quality, false)
+      .addField(`Stats at enhance ${scaledOrb.enhance} and level ${scaledOrb.level} (hp, atk, def, mag)`,
+        '' + scaledOrb.health
+        + ', ' + scaledOrb.attack
+        + ', ' + scaledOrb.defense
+        + ', ' + scaledOrb.magic,
         false)
       .setFooter('Love QL Bot? Check out the about command for more good stuff!',
         'https://questland-discord-bot.cfapps.io/ql_weasel.png');
@@ -155,6 +215,6 @@ const printOrb = (orb) => {
     return { embed };
   } catch (e) {
     console.error(e);
-    return 'Unable to format orb data.';
+    return 'Unable to format scaledOrb data.';
   }
 };
