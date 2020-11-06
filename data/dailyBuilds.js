bccd893f37edd0f02ddeee456f8de73679734c79
Mandalorian007 <mandalorian007@gmail.com>
2020-11-06 10:25:19 -0500

    update builds

--- data/dailyBuilds.js
+++ data/dailyBuilds.js
@@ -4,8 +4,8 @@ const { qlBotUrl } = require("../helpers/constants");
 exports.dailyStandard = {
   name: 'The Daily Standard',
   weapons: 'Anchor of the Damned, Thunderclap',
-  mainHandAlternatives: 'Red Hoarder: Hecatombus',
-  offHandAlternatives: 'Support for Destroyers: Azazel Shield, Green Crystal Shield, Nightmare Throne Shield, Bone Driller, Sacrosanctus Ward',
+  mainHandAlternatives: 'Red Hoarder: Witchfire, Hecatombus',
+  offHandAlternatives: 'Support for Destroyers: Jack o\'Lantern\'s Sting, Azazel Shield, Green Crystal Shield, Nightmare Throne Shield, Bone Driller, Sacrosanctus Ward',
   talent1: 'Sword Wielding',
   talent2: 'Transcendental Tornado',
   talent3: 'Crest Guardian',
@@ -15,8 +15,8 @@ exports.dailyStandard = {
 
 exports.whiteout = {
   name: 'Whiteout',
-  weapons: 'Divine Avenger, Flesheater',
-  mainHandAlternatives: 'Cherry On Top: Combuster, Divine Intervention, Annihilator',
+  weapons: 'Divine Avenger, BladeSong',
+  mainHandAlternatives: 'Cherry On Top: Flesheater, Combuster, Divine Intervention, Annihilator',
   offHandAlternatives: 'Spontaneous Spirits: Iron Bat, Ashes to Ashes Ward',
   talent1: 'Berserker/Bloodlust',
   talent2: 'any',
@@ -28,8 +28,8 @@ exports.whiteout = {
 exports.intenseSwordWielding = {
   name: 'The Intense Sword Wielding',
   weapons: 'Anchor of the Damned, Thunderclap',
-  mainHandAlternatives: 'Red Hoarder: Hecatombus',
-  offHandAlternatives: 'Support for Destroyers: Azazel Shield, Green Crystal Shield, Nightmare Throne Shield, Bone Driller, Sacrosanctus Ward',
+  mainHandAlternatives: 'Red Hoarder: Witchfire, Hecatombus',
+  offHandAlternatives: 'Support for Destroyers: Jack o\'Lantern\'s Sting, Azazel Shield, Green Crystal Shield, Nightmare Throne Shield, Bone Driller, Sacrosanctus Ward',
   talent1: 'Sword Wielding',
   talent2: 'Firebird',
   talent3: 'Crest Guardian',
@@ -41,7 +41,7 @@ exports.flamingShield = {
   name: 'The Flaming Shield (SIBB Alternative)',
   weapons: 'Ratchet Hatchet, Thunderclap',
   mainHandAlternatives: 'Red Blast & Blue Twist: The Last Wish',
-  offHandAlternatives: 'Support for Destroyers: Azazel Shield, Green Crystal Shield, Nightmare Throne Shield, Bone Driller, Sacrosanctus Ward',
+  offHandAlternatives: 'Support for Destroyers: Jack o\'Lantern\'s Sting, Azazel Shield, Green Crystal Shield, Nightmare Throne Shield, Bone Driller, Sacrosanctus Ward',
   talent1: 'Inner Fire',
   talent2: 'Crystal Chains',
   talent3: 'Elevation',
