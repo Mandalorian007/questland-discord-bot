6134d24d6851be8e335f36faac3be5edafdbb0a5
Mandalorian007 <mandalorian007@gmail.com>
2021-08-06 09:11:45 -0400

    remove hard and guild boss stats since they were manually maintained and Gamesture changed everything

--- commands/default.js
+++ commands/default.js
@@ -6,11 +6,10 @@ exports.describe = 'Default Help';
 exports.builder = {};
 
 exports.handler = asyncHandler(async (argv) => {
-  //TODO Community tools like MonsterSlayer stuff could be a section :tools:
   return new Discord.MessageEmbed()
     .setTitle('QL Bot command list')
     .setDescription('For any command just add `-h` or `--help` Ex: ```!ql item --help```')
-    .addField(':card_box: Index Data', ['`item`', '`orb`', '`hard-boss`', '`guild-boss`'], true)
+    .addField(':card_box: Index Data', ['`item`', '`orb`'], true)
     .addField(':flower_playing_cards: Profile Lookups', ['`guild`', '`hero`'], true)
     .addField(':crossed_swords: Community Builds', ['`build`', '`gear-template`', '`daily-boss`'], true)
     .addField(':tools: Community Tools', ['`monster-slayer`', '`hall-of-fame`', '`guild-ranking`'], true)
